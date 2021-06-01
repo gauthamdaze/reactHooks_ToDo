8020a6333c94e14891efd6a8a16a51e83d272923
Gautham Elango <43374544+gauthamdaze@users.noreply.github.com>
2021-06-01 14:06:07 +0530

    Updates

---
+++ precache-manifest.88c7c4b1172052c51902e6dc4e66cb5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dabf62917c9391d8c4cddc5e36e2420c",
+    "url": "/todo-with-hooks/index.html"
+  },
+  {
+    "revision": "4d4fb8b58bdb28817593",
+    "url": "/todo-with-hooks/static/css/main.550237c4.chunk.css"
+  },
+  {
+    "revision": "8918d0af2062251e56c3",
+    "url": "/todo-with-hooks/static/js/2.fb5ada59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-with-hooks/static/js/2.fb5ada59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d4fb8b58bdb28817593",
+    "url": "/todo-with-hooks/static/js/main.a9552928.chunk.js"
+  },
+  {
+    "revision": "189e4b69bcd8f875f15a",
+    "url": "/todo-with-hooks/static/js/runtime-main.1e9ef7cd.js"
+  },
+  {
+    "revision": "4f646371b2a2501c7acd9110625993ca",
+    "url": "/todo-with-hooks/static/media/background.4f646371.jpg"
+  }
+]);
